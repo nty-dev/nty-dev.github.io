6efc632513a654205fdeb70ac49e873473299408
Neo Tsay Yong <padricneo@gmail.com>
2023-07-27 19:28:19 +0800

    Updates

---
+++ precache-manifest.7cbd9c5e4d1b9cc0dba7d1b042aeb48b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd967bcf6a9e0e2753e80ee4b4bba8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd8e863b754ef50343e8",
+    "url": "/static/css/2.56e25c79.chunk.css"
+  },
+  {
+    "revision": "166f1d62c4b348d4f7e4",
+    "url": "/static/css/main.8016e150.chunk.css"
+  },
+  {
+    "revision": "cd8e863b754ef50343e8",
+    "url": "/static/js/2.13468b79.chunk.js"
+  },
+  {
+    "revision": "9e33ea807c256c32d3de3b014ecb5cab",
+    "url": "/static/js/2.13468b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166f1d62c4b348d4f7e4",
+    "url": "/static/js/main.df87e10e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  },
+  {
+    "revision": "a724c781a93678b7705d49b0eccdcf00",
+    "url": "/static/media/Vegas_Pro_15.0.a724c781.png"
+  },
+  {
+    "revision": "9266d4747f9e9debe90bdb98e0b85105",
+    "url": "/static/media/content_writing.9266d474.png"
+  },
+  {
+    "revision": "fe5e1a29ad86aea8f09809c6c52fd99e",
+    "url": "/static/media/minecraft.fe5e1a29.png"
+  },
+  {
+    "revision": "42d37de773b537dca45eac0a5cad2ca0",
+    "url": "/static/media/obs.42d37de7.png"
+  },
+  {
+    "revision": "94a49fe8963ace31b54b41880d2d8854",
+    "url": "/static/media/papermc.94a49fe8.png"
+  }
+]);
